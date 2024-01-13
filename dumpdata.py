f3e4532f0bcb07c528fe1252060fa4d3cc92cec0
nikachu2012 <75615396+nikachu2012@users.noreply.github.com>
2024-01-13 11:19:08 +0900

    [add] アクション選択とkeyA/B選択を追加

--- dumpdata.py
+++ dumpdata.py
@@ -6,7 +6,6 @@ from pathlib import Path
 import smartcard.System
 import smartcard.CardMonitoring
 from smartcard.CardRequest import CardRequest
-from smartcard.CardType import CardType
 from smartcard.util import toHexString, toBytes
 
 # made lib
@@ -17,25 +16,19 @@ if __name__ == "__main__":
     desc = "Created by nikachu2012"
 
     parser = argparse.ArgumentParser(prog="dumpdata.py", description=f"{title}\n{desc}")
-    parser.add_argument(
-        "-l", "--list", help="Print usable reader(s) list.", action="store_true"
-    )
-
+    parser.add_argument("-l", "--list", help="Print usable reader(s) list.", action="store_true")
     parser.add_argument("-v", "--version", action="version", version="version 1.0")
-
+    parser.add_argument("--audio", help="Play wav audio while waiting for card read. (Windows only)", type=Path)
+    parser.add_argument("-r", "--reader", type=int, default=0, help="Select reader index number. (You can get the number from the --list)")
     parser.add_argument(
-        "--audio",
-        help="Play wav audio while waiting for card read. (Windows only)",
-        type=Path,
+        "-a", "--action",type=str, help="Select action.", choices=["display", "dump", "write"]
     )
-
     parser.add_argument(
-        "--reader",
-        type=int,
-        default=0,
-        help="Select reader index number. (You can get the number from the --list)",
+        "-kA", "--keyA",type=str, default="FFFFFF", help="Input keyA.", nargs="*"
     )
-
+    parser.add_argument(
+        "-kB", "--keyB",type=str, default="FFFFFF", help="Input keyB.", nargs="*"
+    )
     args = parser.parse_args()
 
     print(title)
@@ -64,6 +57,7 @@ if __name__ == "__main__":
                 print(f"[{index}]: {elements}")
 
         sys.exit(0)
+
     elif args.reader != None:
         print("Card reading...  ")
         print("TIPS: Timeout in 30 seconds.")
@@ -88,7 +82,20 @@ if __name__ == "__main__":
             )
             print(f"UID: {toHexString(data)}")
 
-            
+            if args.action == None:
+                print("Not found action.")
+                sys.exit(0)
 
+            elif args.action == "dump":
+                if args.keyA:
+                    target = args.keyA
+                elif args.keyB:
+                    target = args.keyB
+                else:
+                    print("Not found keyA/B")
+                    sys.exit(0)
+                
+                
+                pass
         else:
             print(f"Selected reader({args.reader}) can't connect.")
