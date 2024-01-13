e7ef414d05a6a2997a38373be2aeb238e8f0187c
nikachu2012 <75615396+nikachu2012@users.noreply.github.com>
2024-01-13 21:14:12 +0900

    [delete] dumpdata.pyを削除

--- dumpdata.py
+++
@@ -1,6 +0,0 @@
-from smartcard.util import toHexString, toBytes
-from sendAPDU import sendAPDU
-
-def dumpdata(key: list[str], isAllAuth: bool):
-    targetKey = list(map(toBytes, key))
-    print(targetKey)
