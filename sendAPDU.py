ed3ac94e2b7c4cb37fcd1b08a423b64060f15475
nikachu2012 <75615396+nikachu2012@users.noreply.github.com>
2024-01-13 21:13:55 +0900

    [add] ログを残すAPDU送信関数を追加

--- sendAPDU.py
+++ sendAPDU.py
@@ -1,3 +1,5 @@
+from smartcard.util import toHexString, toBytes
+from logger import log
 
 # f = open("mifare_tools.log", mode="a")
 
@@ -13,3 +15,23 @@ def sendAPDU(data:list[int], connection=None) -> tuple:
         except Exception as e:
             print("\033[31mERROR - Couldn't send APDU\033[0m")
             raise e
+
+def sendAPDUwithLOG(data:list[int], logObj, connection=None, ) -> tuple:
+    log(logObj, f"sendAPDU-status: Card sending... (APDU: {toHexString(data)})")
+    try:
+        result = connection.transmit(
+            data
+        )
+
+        if result[1] == 0x90 and result[2] == 0x00:
+            log(logObj, f"sendAPDU-status: Card send success! (SW1: {hex(result[1])}, SW2: {hex(result[2])}, Response: {toHexString(result[0])})")
+            return result
+        else:
+            log(logObj, f"sendAPDU-WARNING: Card result error (SW1: {hex(result[1])}, SW2: {hex(result[2])}, Response: {toHexString(result[0])})")
+            return None
+
+    except Exception as e:
+        print("\033[31mERROR - Couldn't send APDU\033[0m")
+        log(logObj, f"!sendAPDU-Error: Couldn't send APDU.")
+
+        raise e
